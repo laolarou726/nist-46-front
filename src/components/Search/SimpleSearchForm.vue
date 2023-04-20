1f6bfb51ed7d3026bf8402564ae212f4d804bc18
laolarou <luolinrui726@outlook.com>
2023-04-19 21:51:53 -0700

    minor fix

--- src/components/Search/SimpleSearchForm.vue
+++ src/components/Search/SimpleSearchForm.vue
@@ -1,18 +1,29 @@
 <template>
-  <v-text-field
-    class="pt-10"
-    density="compact"
-    variant="solo"
-    label="Search Ligands"
-    append-inner-icon="mdi-magnify"
-    single-line
-    hide-details
-    v-model:model-value="ligandsValue"
-    @input="onLigandsUpdate"
-    @click:append-inner="searchLigands"
-    v-on:keyup.enter="searchLigands"
-    :loading="isLoading ?? false"
-  ></v-text-field>
+  <v-container class="pt-8">
+    <v-text-field
+      class="pt-10"
+      density="compact"
+      variant="solo"
+      label="Search Ligands"
+      append-inner-icon="mdi-magnify"
+      single-line
+      hide-details
+      v-model:model-value="ligandsValue"
+      @input="onLigandsUpdate"
+      @click:append-inner="searchLigands"
+      v-on:keyup.enter="searchLigands"
+      :loading="isLoading ?? false"
+    ></v-text-field>
+    <div class="pa-4 text-end">
+      <v-btn
+        @click="searchLigands"
+        class="text-none"
+        color="primary"
+        min-width="92">
+        Search
+      </v-btn>
+    </div>
+  </v-container>
 </template>
 
 <script lang="ts">
